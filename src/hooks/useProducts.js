6982d8b3cde0229fc589fbb9679ab755e461865d
mdsiamul <mdsiamulsuratprince@gmail.com>
2022-08-02 23:44:58 +0600

    hookc

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,3 +1,12 @@
-const useProducts = () => {
+import { useEffect, useState } from "react";
 
+const useProducts = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+    return [products, setProducts]
 }
+export default useProducts;
